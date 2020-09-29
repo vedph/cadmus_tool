e1b1cacdc244ee5c0ae76a88280168b465c0e03a
Myrmex <dfusi@hotmail.com>
2020-09-29 17:12:11 +0200

    Initial commit

---
+++ cadmus-tool/Services/StandardItemIndexFactoryProvider.cs
@@ -0,0 +1,69 @@
+﻿using Cadmus.Index.Config;
+using Cadmus.Index.Sql;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using System;
+using System.Reflection;
+
+namespace CadmusTool.Services
+{
+    /// <summary>
+    /// Standard item index writer factory provider.
+    /// </summary>
+    /// <seealso cref="IItemIndexFactoryProvider" />
+    public sealed class StandardItemIndexFactoryProvider :
+        IItemIndexFactoryProvider
+    {
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// Initializes a new instance of the
+        /// <see cref="StandardItemIndexFactoryProvider"/> class.
+        /// </summary>
+        /// <param name="connectionString">The connection string.</param>
+        /// <exception cref="ArgumentNullException">connectionString</exception>
+        public StandardItemIndexFactoryProvider(string connectionString)
+        {
+            _connectionString = connectionString ??
+                throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        /// <summary>
+        /// Gets the part/fragment seeders factory.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        /// <returns>Factory.</returns>
+        /// <exception cref="ArgumentNullException">profile</exception>
+        public ItemIndexFactory GetFactory(string profile)
+        {
+            if (profile == null)
+                throw new ArgumentNullException(nameof(profile));
+
+            // build the container for seeders
+            Assembly[] indexAssemblies = new[]
+            {
+                // Cadmus.Index.Sql
+                typeof(MySqlItemIndexWriter).Assembly
+            };
+
+            Container container = new Container();
+
+            ItemIndexFactory.ConfigureServices(
+                container,
+                indexAssemblies);
+
+            container.Verify();
+
+            // load seed configuration
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddInMemoryJson(profile);
+            var configuration = builder.Build();
+
+            return new ItemIndexFactory(
+                container,
+                configuration,
+                _connectionString);
+        }
+    }
+}
---
+++ cadmus-tool/Services/StandardPartSeederFactoryProvider.cs
@@ -0,0 +1,57 @@
+﻿using Cadmus.Core.Config;
+using Cadmus.Seed;
+using Cadmus.Seed.Parts.General;
+using Cadmus.Seed.Philology.Parts.Layers;
+using Fusi.Microsoft.Extensions.Configuration.InMemoryJson;
+using Microsoft.Extensions.Configuration;
+using SimpleInjector;
+using System;
+using System.Reflection;
+
+namespace CadmusTool.Services
+{
+    /// <summary>
+    /// Standard seeders service.
+    /// </summary>
+    public sealed class StandardPartSeederFactoryProvider : IPartSeederFactoryProvider
+    {
+        /// <summary>
+        /// Gets the part/fragment seeders factory.
+        /// </summary>
+        /// <param name="profile">The profile.</param>
+        /// <returns>Factory.</returns>
+        /// <exception cref="ArgumentNullException">profile</exception>
+        public PartSeederFactory GetFactory(string profile)
+        {
+            if (profile == null)
+                throw new ArgumentNullException(nameof(profile));
+
+            // build the tags to types map for parts/fragments
+            Assembly[] seedAssemblies = new[]
+            {
+                // Cadmus.Seed.Parts
+                typeof(NotePartSeeder).Assembly,
+                // Cadmus.Seed.Philology.Parts
+                typeof(ApparatusLayerFragmentSeeder).Assembly
+            };
+            TagAttributeToTypeMap map = new TagAttributeToTypeMap();
+            map.Add(seedAssemblies);
+
+            // build the container for seeders
+            Container container = new Container();
+            PartSeederFactory.ConfigureServices(
+                container,
+                new StandardPartTypeProvider(map),
+                seedAssemblies);
+
+            container.Verify();
+
+            // load seed configuration
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                .AddInMemoryJson(profile);
+            var configuration = builder.Build();
+
+            return new PartSeederFactory(container, configuration);
+        }
+    }
+}
